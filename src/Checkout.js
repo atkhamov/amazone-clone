7c128d367f7d407f9fd25a1cb5d2af9ff02e4286
atkhamov <atkhamov@gmail.com>
2022-09-11 01:30:08 +0600

    add checkoutproduct js and css files

--- src/Checkout.js
+++ src/Checkout.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import "./Checkout.css";
+import { useStateValue } from './StateProvider';
+import CheckoutProduct from './CheckoutProduct';
 import Subtotal from './Subtotal';
 
 function Checkout() {
+    const [{ basket }, dispatch] = useStateValue();
+
     return (
         <div className='checkout'>
             <div className='checkout__left'>
@@ -10,6 +14,17 @@ function Checkout() {
 
                 <div>
                     <h2 className='checkout__title'>Your shopping basket</h2>
+
+                    {basket.map(item => (
+                        <CheckoutProduct
+                            id={item.id}
+                            title={item.title}
+                            image={item.image}
+                            price={item.price}
+                            rating={item.rating}
+                        />
+                    ))}
+                    {/* CheckoutProduct */}
                 </div>
             </div>
 
